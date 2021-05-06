c84a50eb2184b6a84ec374ee663c0556fb413897
gazala ansari <55708169+gazala-ansari@users.noreply.github.com>
2021-05-07 03:01:26 +0530

    Delete n4.py

--- n4.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-client =MongoClient("mongodb+srv://Gazala:Gazala%40123@cluster0.nhjog.mongodb.net/chatbot?retryWrites=true&w=majority")
-db = client.test
-db=client.get_database('chatbot')
-record=db.Exam
-#print(record.find_one({"dept":"computers"},{"subject":1}))
-oo=record.find_one({"dept":"computers"},{"subject":1})
-oo.pop('_id')
-print(oo)
