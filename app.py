97ac4972fd0c36a7a6bce19c52471181a8c38905
gazala ansari <55708169+gazala-ansari@users.noreply.github.com>
2021-05-07 03:23:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():   
     #return render_template("clerk_menu.html") 
-    return render_template("/templates/index.html") 
+    return render_template("index.html") 
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
